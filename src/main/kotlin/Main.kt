ca3e7664a2a30eae0ae7a473172299afb5131db7
Dan Cruz <dancruz.violin@gmail.com>
2024-03-13 21:02:18 +0100

    Refac_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,26 +12,16 @@ fun main() {
         }
         println()
     }
-    var f: Int = 0
-    var c: Int = 0
-
-    while (f < m.size){
-        while (c < m[0].size){
-            sumaFilas[f] += m[f][c]
-            c++
-        }
-        c = 0
-        f++
-    }
-
+    sumarFilas(m, sumaFilas)
     sumarColumnas(m,sumaColum)
+
     println("La suma de las filas es")
     println(Arrays.toString(sumaFilas))
     println("La suma de las columnas es")
     println(Arrays.toString(sumaColum))
 }
 
-fun sumarColumnas(m:Array<Array<Int>>, sumaColumnas:Array<Int>){
+private fun sumarColumnas(m:Array<Array<Int>>, sumaColumnas:Array<Int>){
     var f: Int = 0
     var c: Int = 0
 
@@ -42,5 +32,19 @@ fun sumarColumnas(m:Array<Array<Int>>, sumaColumnas:Array<Int>){
         }
         f = 0
         c++
+    }
+}
+
+private fun sumarFilas(m: Array<Array<Int>>, sumaFilas: Array<Int>) {
+    var f: Int = 0
+    var c: Int = 0
+
+    while (f < m.size) {
+        while (c < m[0].size) {
+            sumaFilas[f] += m[f][c]
+            c++
+        }
+        c = 0
+        f++
     }
 }
